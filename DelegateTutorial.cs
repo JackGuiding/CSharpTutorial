59b5dd2566ff1bbc2c3b8c64e094106d2d22d791
Jackutea <chenwansal1@163.com>
2024-04-09 21:13:54 +0800

    <feature> add: 官方把 无参数~T16泛型参数的 delegate Action 都写好了

--- DelegateTutorial.cs
+++ DelegateTutorial.cs
@@ -1,39 +1,55 @@
 using System;
 
+// 官方做的事:
+// public delegate void Action();
+// public delegate void Action<T>(T t1);
+// public delegate void Action<T1, T2>(T1 t1, T2 t2);
+// ...... <T1, ..., T16>
+
 // public delegate void Action(); // 官方委托, 所有返回值为 void, 且无参数的 delegate 都可以用 Action
 // public delegate void MyAction(); 没必要使用 
 // public delegate void YoAction(); 没必要使用
 
-public delegate void Dele2(int a);
-public delegate int Dele3(float f);
+public delegate void Action<T>(T a); // 官方的
+public delegate void ActionT<T>(T a); // 可替代下面两个
+public delegate void Action1(int a);
+public delegate void Action2(float a);
+
+public delegate int Func3(float f);
 
 public static class DelegateTutorial {
 
     public static void Entry() {
 
-        Action a1 = Func1;
-        a1.Invoke();
-
-        Action d1 = Func1;
-        d1.Invoke();
+        // ==== Eg: 1a ====
+        Action1 action1 = AMethod1;
+        action1.Invoke(5);
         // Func1();
 
-        Dele2 d2 = Func2;
-        d2.Invoke(5);
+        Action2 action2 = AMethod2;
+        action2.Invoke(50.8f);
         // Func2(5);
 
-        Dele3 d3 = Func3;
+        // ==== Eg: 1b ====
+        ActionT<int> t1 = AMethod1;
+        t1.Invoke(6);
+
+        ActionT<float> t2 = AMethod2;
+        t2.Invoke(5.3f);
+
+        // ==== Eg: 2 ====
+        Func3 d3 = Func3;
         int v3 = d3.Invoke(2.5f);
         System.Console.WriteLine("v3: " + v3);
         // int v3 = Func3(2.5f);
 
     }
 
-    static void Func1() {
-        Console.WriteLine("Func1");
+    static void AMethod1(int val) {
+        Console.WriteLine("Func1: " + val);
     }
 
-    static void Func2(int val) {
+    static void AMethod2(float val) {
         Console.WriteLine("Func2: " + val);
     }
 
